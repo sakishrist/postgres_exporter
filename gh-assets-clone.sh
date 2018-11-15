4f3342fa8b1d4400a4c69037491a37db5c15a3ce
Miguel Hernandez <mhernandez401405@gmail.com>
2018-11-15 10:03:10 -0800

    print statements for debugging

--- gh-assets-clone.sh
+++ gh-assets-clone.sh
@@ -3,11 +3,16 @@
 # GIT_API_KEY.
 
 [ ! -z "$GIT_ASSETS_BRANCH" ] || exit 1
+echo "GIT_ASSETS_BRANCH passed"
+[ ! -z "$GIT_API_KEY" ] || exit 1
+echo "GIT_API_KEY passed"
 
+echo "Beginning git setup..."
 setup_git() {
   git config --global user.email "travis@travis-ci.org" || exit 1
   git config --global user.name "Travis CI" || exit 1
 }
+echo "setup_git() passed"
 
 # Constants
 ASSETS_DIR=".assets-branch"
@@ -15,4 +20,4 @@ ASSETS_DIR=".assets-branch"
 # Clone the assets branch with the correct credentials
 git clone --single-branch -b "$GIT_ASSETS_BRANCH" \
     "https://${GIT_API_KEY}@github.com/${TRAVIS_REPO_SLUG}.git" "$ASSETS_DIR" || exit 1
-
+echo "git clone GIT_ASSETS_BRANCH passed"
